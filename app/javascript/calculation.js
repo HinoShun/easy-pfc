bef1daed4c210e2cd593d5e967c1ffe09fca2e25
HinoShun <shumpei.hino88@icloud.com>
2023-04-30 16:33:36 +0900

    Ajax通信：Jsからのリクエスト実装

--- app/javascript/calculation.js
+++ app/javascript/calculation.js
@@ -1,5 +1,16 @@
 function calculation (){
-  console.log("OK")
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    
+    const form = document.getElementById("form");
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+
+    XHR.open("POST", "/body_data", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+  });
 }
  
  window.addEventListener('load', calculation);
