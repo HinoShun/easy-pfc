2de20982b9ff6473ee241238f806f329e3dacc87
HinoShun <shumpei.hino88@icloud.com>
2023-05-02 17:40:13 +0900

    calculation.js 変更（関数未作成）

--- app/javascript/calculation.js
+++ app/javascript/calculation.js
@@ -1,40 +1,55 @@
 const buildHTML = (XHR) => {
   const item = XHR.response.body_datum;
+  const BMR = 100; //BMR：基礎代謝
+  const burnCalories = calcBurnCalories();
+  const intakeCalories = 150;
+  const proteinAmount = 100;
+  const proteinCalories = 100;
+  const carbAmount = 100;
+  const carbCalories = 100;
+  const fatAmount = 100;
+  const fatCalories = 100;
+  
   const html = `
     <div class="col_">
       <div class="">基礎代謝</div>
-      <div class="">kcal</div>
+      <div class="">${BMR}kcal</div>
     </div>
 
     <div class="col_">
       <div class="">消費カロリー</div>
-      <div class="">kcal</div>
+      <div class="">${burnCalories}kcal</div>
     </div>
 
     <div class="col_">
       <div class="">目標摂取カロリー</div>
-      <div class="">kcal</div>
+      <div class="">${intakeCalories}kcal</div>
     </div>
 
     <div class="col_">
       <div class="">P(タンパク質)</div>
-      <div class="">g</div>
-      <div class="">kcal</div>
+      <div class="">${proteinAmount}g</div>
+      <div class="">${proteinCalories}kcal</div>
     </div>
 
     <div class="col_">
       <div class="">C(炭水化物)</div>
-      <div class="">g</div>
-      <div class="">kcal</div>
+      <div class="">${carbAmount}g</div>
+      <div class="">${carbCalories}kcal</div>
     </div>
 
     <div class="col_">
       <div class="">F(脂質)</div>
-      <div class="">g</div>
-      <div class="">kcal</div>
+      <div class="">${fatAmount}g</div>
+      <div class="">${fatCalories}kcal</div>
     </div>`;
   return html;
 };
+
+function calcBurnCalories(){
+  const burnCalories = 300;
+  return burnCalories
+}
 
 
 function calculation (){
