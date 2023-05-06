e4a7d115604a1e3d92925c961fca35a687cb59df
HinoShun <shumpei.hino88@icloud.com>
2023-05-06 19:05:25 +0900

    function calcBMR作成,HTML,CSS修正

--- app/javascript/calculation.js
+++ app/javascript/calculation.js
@@ -1,6 +1,6 @@
 const buildHTML = (XHR) => {
-  const item = XHR.response.body_datum;
-  const BMR = 100; //BMR：基礎代謝
+  const bodyDatum = XHR.response.body_datum;
+  const BMR = calcBMR(bodyDatum.sex_id, bodyDatum.weight, bodyDatum.height, bodyDatum.age); //BMR：基礎代謝
   const burnCalories = calcBurnCalories();
   const intakeCalories = 150;
   const proteinAmount = 100;
@@ -11,40 +11,51 @@ const buildHTML = (XHR) => {
   const fatCalories = 100;
   
   const html = `
-    <div class="col_">
+    <div class="col-sm-4 mx-auto mt-3">
       <div class="">基礎代謝</div>
       <div class="">${BMR}kcal</div>
     </div>
 
-    <div class="col_">
+    <div class="col-sm-4 mx-auto mt-3">
       <div class="">消費カロリー</div>
       <div class="">${burnCalories}kcal</div>
     </div>
 
-    <div class="col_">
+    <div class="col-sm-4 mx-auto mt-3">
       <div class="">目標摂取カロリー</div>
       <div class="">${intakeCalories}kcal</div>
     </div>
 
-    <div class="col_">
+    <div class="col-sm-4 mx-auto mt-3">
       <div class="">P(タンパク質)</div>
       <div class="">${proteinAmount}g</div>
       <div class="">${proteinCalories}kcal</div>
     </div>
 
-    <div class="col_">
+    <div class="col-sm-4 mx-auto mt-3">
       <div class="">C(炭水化物)</div>
       <div class="">${carbAmount}g</div>
       <div class="">${carbCalories}kcal</div>
     </div>
 
-    <div class="col_">
+    <div class="col-sm-4 mx-auto mt-3">
       <div class="">F(脂質)</div>
       <div class="">${fatAmount}g</div>
       <div class="">${fatCalories}kcal</div>
     </div>`;
   return html;
 };
+
+function calcBMR(sex, weight, height, age){
+  if(sex === 2){
+    BMR = 13.397 * weight + 4.799 * height - 5.677 * age + 88.362;
+  }
+  if(sex === 3){
+    BMR = 9.247 * weight + 3.098 * height - 4.33 * age + 447.593;
+  }
+  return BMR
+}
+
 
 function calcBurnCalories(){
   const burnCalories = 300;
@@ -74,7 +85,7 @@ function calculation (){
       const list = document.getElementById("list");
       list.insertAdjacentHTML("afterend", buildHTML(XHR));
 
-      formText.value = "";
+      //formText.value = "";
     };
   });
 }
